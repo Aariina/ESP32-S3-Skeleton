7d46fee80b7820d0b85a3869d5a3df93ed01810c
Aariina <arinamiakhail@gmail.com>
2025-04-14 12:39:50 +0200

    Update project.ino

--- project/project.ino
+++ project/project.ino
@@ -13,8 +13,8 @@
 
 
 // Remember to remove these before commiting in GitHub
-String ssid = "ssid";
-String password = "password";
+String ssid = "användarnamn";Arina nät
+String password = "password";nononi123
 
 // "tft" is the graphics libary, which has functions to draw on the screen
 TFT_eSPI tft = TFT_eSPI();
